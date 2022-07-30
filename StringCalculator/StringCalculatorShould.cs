1923f1367bd03d556a50daebbad356f239dcf257
Joaquin Navarro <inavarrojoaquin@gmail.com>
2022-07-30 11:34:26 +0200

    Added Multiple single-length separators

--- StringCalculator/StringCalculatorShould.cs
+++ StringCalculator/StringCalculatorShould.cs
@@ -64,5 +64,11 @@ namespace StringCalculator
         {
             Assert.AreEqual(result, new Calculator().Add(input));
         }
+
+        [TestCase("//[*][%]\n1*2%3", 6)]
+        public void AddNumbersWithMultipleSingleLenghtSeparator(string input, int result)
+        {
+            Assert.AreEqual(result, new Calculator().Add(input));
+        }
     }
 }
